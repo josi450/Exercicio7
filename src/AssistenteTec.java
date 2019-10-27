8344d61125b10d9382c98bfc06490279ed5c0a0d
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 15:44:06 -0300

    Update and rename Aviao.java to AssistenteTec.java

---
+++ src/AssistenteTec.java
@@ -0,0 +1,21 @@
+public class AssistenteTec extends Assistente {
+    
+    private double bonus;
+
+    public AssistenteTec (String n, String m, String nd, String s, double b){
+        super (n, m, nd, s);
+        this.setBonus(b);
+    }
+    
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    
+    
+    
+}
+© 2019 GitHub, Inc.
