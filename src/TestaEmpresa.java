39b37b084d8a6704ca7526f1bdcab0252233c293
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 16:09:11 -0300

    Update TestaEmpresa.java

--- src/TestaEmpresa.java
+++ src/TestaEmpresa.java
@@ -1,13 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author josia
- */
 public class TestaEmpresa {
-
+    
+    public static void main(String[] args) {
+        
+        Gerente g1 = new Gerente ("João", "12345", "BCSI"); 
+        
+        AssistenteAdm aa1 = new AssistenteAdm ("Josi", "4569", "BCSI", g1, 0.25);
+        
+        AssistenteTec at1 = new AssistenteTec ("Maria", "9956", "BCSI", g1 , 1236.52);
+        
+        System.out.println(aa1.getNome());
+        System.out.println(aa1.getMatricula());
+        System.out.println(g1.getNomedep());
+        System.out.println(g1.getNome());
+        System.out.println(aa1.getAdnoturno());
+        
+        System.out.println(at1.getNome());
+        System.out.println(at1.getMatricula());
+        System.out.println(g1.getNomedep());
+        System.out.println(g1.getNome());
+        System.out.println(at1.getBonus());
+    } 
+    
 }
