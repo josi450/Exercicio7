f44d32ea6bf1fc4ef1dd377fc443af3e63f294ea
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 15:57:22 -0300

    Update Empregado.java

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,13 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author josia
- */
-public class Empregado {
-
+public class Empregado extends Pessoa {
+    
+    private String codigoSetor;
+    private double salarioBase;
+    private double imposto;
+    
+    public Empregado (String n, String e, String t, String cs, double sb, double i){
+        super (n, e, t);
+        this.setCodigoSetor (cs);
+        this.setSalarioBase (sb);
+        this.setImposto (i);
+    }
+    public String getCodigoSetor(){
+        return this.codigoSetor;
+    }
+    public void setCodigoSetor(String cs){
+        this.codigoSetor = cs;
+    }
+    
+    public double getSalarioBase(){
+        return this.salarioBase;
+    }
+    public void setSalarioBase (double sb){
+        this.salarioBase = sb;
+    }
+    
+    public double getImposto(){
+        return this.imposto;
+    }
+    public void setImposto (double i){
+        this.imposto = i;
+    }
+    
+    public double calcularSalario(){
+        return getSalarioBase() - getImposto();
+    }
 }
