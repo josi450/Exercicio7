2f3381e197390be6d0f49d7d334a0f6caed73016
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 14:48:22 -0300

    Update ProfessorRegime.java

--- src/ProfessorRegime.java
+++ src/ProfessorRegime.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author josia
- */
 public class ProfessorRegime {
+private float salario;
+public ProfRegime(String n,String m,int i,float vs)
+{
+super(n,m,i);
+if (vs > 0) salario = vs;
+}
+public float retornaSalario()
+{
+return salario - (salario * 0.16);
+}
 
 }
