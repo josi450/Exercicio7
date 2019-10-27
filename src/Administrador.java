ddd19893452be7d43a2e9b4e9bea48d80d9f5668
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 15:30:24 -0300

    Update Administrador.java

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,13 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author josia
- */
-public class Administrador {
-
+public class Administrador extends Empregado {
+    
+    private double ajudaDeCusto;
+    
+    public Administrador (String n, String e,String t, String cs, double sb, double i, double ac){
+        super (n, e, t, cs, sb, i);
+        this.setAjudaDeCusto (ac);
+        
+    }
+    
+    public double getAjudaDeCusto(){
+        return this.ajudaDeCusto;
+    }
+    public void setAjudaDeCusto (double ac){
+        this.ajudaDeCusto = ac;
+    }
+    
+    public double calcularSalario(){
+        return calcularSalario() + getAjudaDeCusto();
+    }
 }
