cf828c7162a4393aecb1a85c429c1ea1fbea8a56
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 14:49:59 -0300

    Update ProfessorHorista.java

--- src/ProfessorHorista.java
+++ src/ProfessorHorista.java
@@ -1,13 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author josia
- */
 public class ProfessorHorista {
 
+protected float salario_hora;
+protected int total_horas;
+public ProfessorHorista(String n,String m,int i,int h, float vs)
+{
+super(n,m,i);
+if (h > 0) total_horas = h;
+if (vs > 0) salario_hora = vs;
 }
+public float retornaSalario()
+{
+float salario_base = salario_hora * total_horas;
+return salario_base - (salario_base * 0.16);
+}
+}
+
+
