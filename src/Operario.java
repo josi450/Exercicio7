ebabba6d99bb177a65d4e383afa036fd23e597e9
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 16:02:14 -0300

    Update Operario.java

--- src/Operario.java
+++ src/Operario.java
@@ -1,13 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author josia
- */
-public class Operario {
-
+public class Operario extends Empregado {
+    
+    private double valorProd;
+    private double comissao;
+    
+    public Operario (String n, String e, String t, String cs, double sb, double i, double vp, double c){
+        super (n, e, t, cs, sb, i);
+        this.setValorProd (vp);
+        this.setComissao (c);
+    }
+    
+    public double getValorProd(){
+        return this.valorProd;
+    }
+    public void setValorProd (double vp){
+        this.valorProd = vp;
+    }
+    
+    public double getComissao(){
+        return this.comissao;
+    }
+    public void setComissao (double c){
+        this.comissao = c;
+    }
+    
+    public double calcularSalario(){
+        return calcularSalario() + getComissao();
+    }
+    
 }
