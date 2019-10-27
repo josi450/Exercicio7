9ba26dc493a0869c27cbf7fff5f2539a849fdc4a
josi450 <54296285+josi450@users.noreply.github.com>
2019-10-27 14:55:27 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,13 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author josia
- */
 public class Pessoa {
+  private String nome;
+  priavte String endereco;
+  private String telefone;
+  
+  //construtor
+  public Aluno(String n, String e, String t) {
+        this.nome = n;
+        this.endereco = e;
+        this.telefone = t;
+    }
+  public String getNome() {
+        return this.nome;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setNome(String n) {
+        this.nome = n;
+    }
+
+    public void setEndereco(String e) {
+        this.endereco = e;
+    }
+
+    public void setTelefone(String t) {
+        this.telefone = t;
+    }
 
 }
